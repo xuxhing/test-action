436ce770288a0b7fe4628da2b83383d0724a3db0
xiaohong.xu <xiaohong.xu@shareworks.cn>
2023-12-07 20:41:22 +0800

    update

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        String[] array = null;
+        int length = array.length;
+        System.out.println("Array length: " + length);
     }
 }
